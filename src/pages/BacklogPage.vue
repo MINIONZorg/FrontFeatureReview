4784f4a371e516610c6c2f8aee84538dc4cc0d44
chloe_choi <plumxeun@gmail.com>
2024-09-01 20:12:15 +0900

    [Feature] Draggable complete

--- src/pages/BacklogPage.vue
+++ src/pages/BacklogPage.vue
@@ -1,27 +1,46 @@
 <script setup>
-import {ref} from 'vue';
-import { VueDraggableNext } from 'vue-draggable-next';
+import {onMounted, ref} from 'vue';
+import {VueDraggableNext} from 'vue-draggable-next';
 import {
+  AddButton,
   BacklogContainer,
   NewTask,
+  RemoveButton,
   TaskInput,
-  AddButton,
-  TaskList,
   TaskItem,
-  RemoveButton
+  TaskList
 } from '@/style/StyledComponents';
 
-const backlogItems = ref([
-  {id: 0, title: 'Task 1', status: 'todo'},
-  {id: 1, title: 'Task 2', status: 'in-progress'},
-]);
+// const todoList = ref([
+//   {idx: null, title: null, status: null},
+// ]);
+
+// const list = ref({
+//   todo: [{idx: null, title: null, status: null}],
+//   progress: [{idx: null, title: null, status: null}],
+//   done: [{idx: null, title: null, status: null}],
+// });
+const list = ref({
+  todo: [],
+  progress: [],
+  done: [],
+});
+// const progressList = ref([
+//   {idx: null, title: null, status: null},
+// ]);
+//
+// const doneList = ref([
+//   {idx: null, title: null, status: null},
+//
+// ]);
 
 const newTask = ref('');
 
 const addTask = () => {
+
   if (newTask.value.trim()) {
-    backlogItems.value.push({
-      id: backlogItems.value.length,
+    list.value.todo.push({
+      idx: new Date(),
       title: newTask.value.trim(),
       status: 'todo',
     });
@@ -29,50 +48,50 @@ const addTask = () => {
   }
 };
 
-const removeTask = (id) => {
-  backlogItems.value = backlogItems.value.filter(item => item.id !== id);
-};
+// const removeTask = (title) => {
+//   todoList.value = todoList.value.filter(item => item.title !== title);
+// };
 
-const changeStatus = (id, status) => {
-  const task = backlogItems.value.find(item => item.id === id);
-  if (task) {
-    task.status = status;
+// const changeStatus = (title, status) => {
+//   const task = todoList.value.find(item => item.title === title);
+//   if (task) {
+//     task.status = status;
+//   }
+// };
+
+const moveTask = (event, status) => {
+  const {added} = event;
+  if (added) {
+    const {element} = added;
+    element.status = status
   }
 };
 
-const moveTask = (event) => {
-  const {moved} = event;
-  const {element, newIndex} = moved;
-  const task = backlogItems.value.find(item => item && item.id === element.id);
-  if (task) {
-    if (newIndex === document.querySelector('.kanban-column:nth-child(1) .task-list')) {
-      task.status = 'todo';
-    } else if (newIndex === document.querySelector('.kanban-column:nth-child(2) .task-list')) {
-      task.status = 'in-progress';
-    } else if (newIndex === document.querySelector('.kanban-column:nth-child(3) .task-list')) {
-      task.status = 'done';
-    }
-  }
-};
+onMounted(() => {
+})
+
 </script>
 
 <template>
   <BacklogContainer>
     <h2>Backlog</h2>
     <NewTask>
-      <TaskInput v-model="newTask" placeholder="New task" @keyup.enter="addTask"/>
+      <TaskInput v-model="newTask" @keyup.enter="addTask" placeholder="New task"/>
       <AddButton @click="addTask">Add</AddButton>
     </NewTask>
     <div class="kanban-board">
       <div class="kanban-column">
         <h3>To Do</h3>
         <TaskList>
-          <VueDraggableNext v-model="backlogItems" :group="{ name: 'tasks' }" @change="moveTask">
-            <TaskItem v-for="(item) in backlogItems.filter(item => item.status === 'todo')" :key="item.id">
+          <VueDraggableNext v-model="list.todo" :group="{ name: 'tasks'}" @change="moveTask($event,'todo')"
+                            item-index="title">
+            <TaskItem v-for="(item,idx) in list.todo" :key="item.title">
               <span>{{ item.title }}</span>
               <div>
-                <button @click="changeStatus(item.id, 'in-progress')">In Progress</button>
-                <RemoveButton @click="removeTask(item.id)">Remove</RemoveButton>
+                <button @click="item.status = 'in-progress'; list.progress.push(item); list.todo.splice(idx,1)">In
+                  Progress
+                </button>
+                <RemoveButton @click="list.todo.splice(idx,1)">Remove</RemoveButton>
               </div>
             </TaskItem>
           </VueDraggableNext>
@@ -80,29 +99,31 @@ const moveTask = (event) => {
       </div>
       <div class="kanban-column">
         <h3>In Progress</h3>
-        <VueDraggableNext v-model="backlogItems" :group="{ name: 'tasks' }" @change="moveTask">
-          <TaskList>
-            <TaskItem v-for="item in backlogItems.filter(item => item.status === 'in-progress')" :key="item.id">
-              <span>{{ item.title }}</span>
-              <div>
-                <button @click="changeStatus(item.id, 'done')">Done</button>
-                <RemoveButton @click="removeTask(item.id)">Remove</RemoveButton>
-              </div>
-            </TaskItem>
-          </TaskList>
+        <VueDraggableNext v-model="list.progress" :group="{ name: 'tasks'}"
+                          @change="moveTask($event,'in-progress')" item-key="title">
+
+          <TaskItem v-for="(item,idx) in list.progress" :key="item.title">
+            <span>{{ item.title }}</span>
+            <div>
+              <!--              <button @click="changeStatus(item.title, 'done')">Done</button>-->
+              <button @click="item.status = 'done'; list.done.push(item); list.progress.splice(idx,1)">Done</button>
+
+              <RemoveButton @click="list.progress.splice(idx,1)">Remove</RemoveButton>
+            </div>
+          </TaskItem>
+
         </VueDraggableNext>
       </div>
       <div class="kanban-column">
         <h3>Done</h3>
-        <VueDraggableNext v-model="backlogItems" :group="{ name: 'tasks' }" @change="moveTask">
-          <TaskList>
-            <TaskItem v-for="item in backlogItems.filter(item => item.status === 'done')" :key="item.id">
-              <span>{{ item.title }}</span>
-              <div>
-                <RemoveButton @click="removeTask(item.id)">Remove</RemoveButton>
-              </div>
-            </TaskItem>
-          </TaskList>
+        <VueDraggableNext v-model="list.done" :group="{ name: 'tasks' }"
+                          @change="moveTask($event,'done')" item-key="title">
+          <TaskItem v-for="(item,idx) in list.done" :key="item.title">
+            <span>{{ item.title }}</span>
+            <div>
+              <RemoveButton @click="list.done.splice(idx)">Remove</RemoveButton>
+            </div>
+          </TaskItem>
         </VueDraggableNext>
       </div>
     </div>
